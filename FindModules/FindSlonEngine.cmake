8529651545e38c66bbefd75cebf3dda5322c55a9
dikobraz <devnull@localhost>
2009-07-23 08:50:27 +0000

    [*] FindCommonModule support dependencies, fixed FindSlonEngine

--- FindModules/FindSlonEngine.cmake
+++ FindModules/FindSlonEngine.cmake
@@ -1,12 +1,13 @@
 # Find dependencies
-FIND_PACKAGE ( Boost 1.36.0 	REQUIRED	COMPONENTS signals )
+FIND_PACKAGE ( Boost 1.36.0 	REQUIRED	COMPONENTS signals thread )
 FIND_PACKAGE ( XML++            REQUIRED )
 FIND_PACKAGE ( SimpleGL         REQUIRED )
+FIND_PACKAGE ( SDL       		REQUIRED )
 
 # Find SlonEngine
 SET (WANTED_MODULE_NAME     SlonEngine)
 SET (WANTED_INCLUDES        SlonEngine/Engine.h)
 SET (WANTED_LIBRARIES       SlonEngine)
-SET (WANTED_DEPENDENCIES    Boost XML++ SimpleGL)
+SET (WANTED_DEPENDENCIES    Boost XML++ SimpleGL SDL)
 INCLUDE (FindCommonModule)
 
