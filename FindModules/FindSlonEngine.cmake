e7a390351cad58603b85e2744c198a826b736df3
dikobraz <devnull@localhost>
2009-08-27 11:20:41 +0000

    SlonEngine find inprovements

--- FindModules/FindSlonEngine.cmake
+++ FindModules/FindSlonEngine.cmake
@@ -3,11 +3,18 @@ FIND_PACKAGE ( Boost 1.36.0 	REQUIRED	COMPONENTS signals thread )
 FIND_PACKAGE ( XML++            REQUIRED )
 FIND_PACKAGE ( SimpleGL         REQUIRED )
 FIND_PACKAGE ( SDL       		REQUIRED )
+FIND_PACKAGE ( Bullet )
 
 # Find SlonEngine
 SET (WANTED_MODULE_NAME     SlonEngine)
 SET (WANTED_INCLUDES        SlonEngine/Engine.h)
 SET (WANTED_LIBRARIES       SlonEngine)
 SET (WANTED_DEPENDENCIES    Boost XML++ SimpleGL SDL)
+
+# optional dependencies
+IF (BULLET_FOUND)
+    LIST ( APPEND WANTED_DEPENDENCIES Bullet )
+ENDIF (BULLET_FOUND)
+
 INCLUDE (FindCommonModule)
 
