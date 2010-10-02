369c38676c0cc6a748d9c5d7f5e0fc797034819a
dikobraz <devnull@localhost>
2010-10-02 13:55:25 +0000

    * fix dependencies for SimpleGL (2)

--- FindModules/FindSimpleGL.cmake
+++ FindModules/FindSimpleGL.cmake
@@ -1,3 +1,6 @@
+FIND_PACKAGE ( OpenGL       REQUIRED )
+FIND_PACKAGE ( Glew   		REQUIRED )
+
 SET (WANTED_MODULE_NAME     SimpleGL)
 SET (WANTED_INCLUDES        sgl/Device.h)
 SET (WANTED_LIBRARIES       sgl)
