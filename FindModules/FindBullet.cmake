a51468f088019cf1e8a50cd4755dddff9c051673
dikobraz <devnull@localhost>
2011-03-26 17:00:08 +0300

    * fixed FindBullet.cmake for linux

--- FindModules/FindBullet.cmake
+++ FindModules/FindBullet.cmake
@@ -1,46 +1,39 @@
 SET (BULLET_PATHS    
-   $ENV{PROGRAMFILES}/Bullet
    $ENV{PROGRAMFILES}/BULLET_PHYSICS
    $ENV{BULLETDIR}
    $ENV{BULLET_DIR}
 )
 
-IF (WIN32)
-    SET (INCLUDE_PATH_PREFIX "")
-ELSE (WIN32)
-    SET (INCLUDE_PATH_PREFIX "BULLET_PHYSICS/")
-ENDIF (WIN32)
-
 SET (WANTED_MODULE_NAME     BulletMath)
-SET (WANTED_INCLUDES        "${INCLUDE_PATH_PREFIX}/bullet/LinearMath/btVector3.h")
+SET (WANTED_INCLUDES        "bullet/LinearMath/btVector3.h")
 SET (WANTED_LIBRARIES       linearmath bulletmath LinearMath)
 SET (WANTED_INCLUDES_PATH   ${BULLET_PATHS}) 
 SET (WANTED_LIBRARIES_PATH  ${BULLET_PATHS}) 
 INCLUDE (FindCommonModule)
 
 SET (WANTED_MODULE_NAME     BulletCollision)
-SET (WANTED_INCLUDES        "${INCLUDE_PATH_PREFIX}/bullet/btBulletCollisionCommon.h")
+SET (WANTED_INCLUDES        "bullet/btBulletCollisionCommon.h")
 SET (WANTED_LIBRARIES       bulletcollision BulletCollision)
 SET (WANTED_INCLUDES_PATH   ${BULLET_PATHS}) 
 SET (WANTED_LIBRARIES_PATH  ${BULLET_PATHS}) 
 INCLUDE (FindCommonModule)
 
 SET (WANTED_MODULE_NAME     BulletDynamics)
-SET (WANTED_INCLUDES        "${INCLUDE_PATH_PREFIX}/bullet/btBulletDynamicsCommon.h")
+SET (WANTED_INCLUDES        "bullet/btBulletDynamicsCommon.h")
 SET (WANTED_LIBRARIES       bulletdynamics BulletDynamics)
 SET (WANTED_INCLUDES_PATH   ${BULLET_PATHS}) 
 SET (WANTED_LIBRARIES_PATH  ${BULLET_PATHS}) 
 INCLUDE (FindCommonModule)
 
 SET (WANTED_MODULE_NAME     BulletFileLoader)
-SET (WANTED_INCLUDES        "${INCLUDE_PATH_PREFIX}/bullet/Extras/Serialize/BulletFileLoader/btBulletFile.h")
+SET (WANTED_INCLUDES        "bullet/Extras/Serialize/BulletFileLoader/btBulletFile.h")
 SET (WANTED_LIBRARIES       bulletfileloader BulletFileLoader)
 SET (WANTED_INCLUDES_PATH   ${BULLET_PATHS}) 
 SET (WANTED_LIBRARIES_PATH  ${BULLET_PATHS}) 
 INCLUDE (FindCommonModule)
 
 SET (WANTED_MODULE_NAME     BulletWorldImporter)
-SET (WANTED_INCLUDES        "${INCLUDE_PATH_PREFIX}/bullet/Extras/Serialize/BulletWorldImporter/btBulletWorldImporter.h")
+SET (WANTED_INCLUDES        "bullet/Extras/Serialize/BulletWorldImporter/btBulletWorldImporter.h")
 SET (WANTED_LIBRARIES       bulletworldimporter BulletWorldImporter)
 SET (WANTED_INCLUDES_PATH   ${BULLET_PATHS}) 
 SET (WANTED_LIBRARIES_PATH  ${BULLET_PATHS}) 
@@ -52,7 +45,7 @@ IF ( BULLETMATH_FOUND AND BULLETCOLLISION_FOUND AND BULLETDYNAMICS_FOUND )
 ENDIF ( BULLETMATH_FOUND AND BULLETCOLLISION_FOUND AND BULLETDYNAMICS_FOUND )
 
 # correct include path 
-SET (Bullet_INCLUDE_DIR ${BulletMath_INCLUDE_DIR}/${INCLUDE_PATH_PREFIX})
+SET (Bullet_INCLUDE_DIR ${BulletMath_INCLUDE_DIR} ${BulletMath_INCLUDE_DIR}/bullet)
 
 SET (Bullet_LIBRARY
     ${BulletDynamics_LIBRARY}
